d346ec6f6c24444f289b37b8ea54b97c60f2738a
unknown <kinew@LIVPCRU-7.ad.nerc.ac.uk>
2015-10-05 16:47:56 +0100

    papers left by jenny all added

--- auto/kwnphd.el
+++ auto/kwnphd.el
@@ -1,6 +1,19 @@
-(TeX-add-style-hook "kwnphd"
+(TeX-add-style-hook
+ "kwnphd"
  (lambda ()
-    (LaTeX-add-bibitems
-     "brown11"
-     "bolanos13")))
+   (LaTeX-add-bibitems
+    "brown11"
+    "bolanos13"
+    "wolf11"
+    "brown13"
+    "bolanos14"
+    "brown10"
+    "brown09"
+    "polton11"
+    "palmer10"
+    "howarth11"
+    "prandle11"
+    "holt08"
+    "holt01"
+    "holt06")))
 
