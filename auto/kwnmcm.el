4649557604a3b09667378fb1b85baf521e7dac55
K. Newman - Laptop <kinew@noc.ac.uk>
2018-01-04 16:28:08 +0000

    added morecambe bibs

---
+++ auto/kwnmcm.el
@@ -0,0 +1,29 @@
+(TeX-add-style-hook
+ "kwnmcm"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "brown11"
+    "smith96"
+    "burt81"
+    "lee83"
+    "burt83"
+    "rattarangsi90"
+    "tsai95"
+    "glasbey93"
+    "baradez04"
+    "burge09"
+    "ju97"
+    "ganugapati95"
+    "ganugapati96"
+    "zhang11"
+    "hazarika15"
+    "mastriani16"
+    "ju97thesis"
+    "ma14"
+    "montazeri16"
+    "dave15"
+    "choo14"
+    "choo12"
+    "gonzalez01"))
+ :bibtex)
+
